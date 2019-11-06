73817b960c954c8a66e4fe5614d94feea5e549ab
mansurhamidov14 <mansur.hamidoff.1994@gmail.com>
2019-11-07 00:35:42 +0400

    Switcher view - Completed

--- src/components/Switcher/Switcher.tsx
+++ src/components/Switcher/Switcher.tsx
@@ -11,7 +11,11 @@ export const Switcher: React.FC<ISwithcerProps> = ({isActive, onSwitch}) => {
         <div className={styles.switcher}>
             <label>
                 <input type="checkbox" className={styles.switcher__checkbox} checked={isActive} onChange={onSwitch}/>
-                <span className={styles.switcher__status}/>
+                <span className={styles.switcher__status}>
+                    <span className={styles.switcher__status__on}>&#10004;</span>
+                    <span className={styles.switcher__status__off}/>
+                    <span className={styles.switcher__status__main}></span>
+                </span>
             </label>
         </div>
     )
